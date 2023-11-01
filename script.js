1369e6d11b9dc35647e9ab8e61f1bcc2b253c5e5
Keshujangid <keshujangid191@gmail.com>
2023-11-01 08:05:14 +0530

    Change Object constructor to class

--- script.js
+++ script.js
@@ -12,26 +12,22 @@ showForm.addEventListener('click', () => {
   dialog.showModal()
 })
 
-closeButton.addEventListener('click', () => {
-  // for (let i = 0; i < inputs.length - 1; i++) {
-  //   inputs[i].value = ''
-  // }
-  // inputs[3].checked = false
-  // dialog.close();
+closeButton.addEventListener('click', (event) => {
   clearInput();
+
+  event.preventDefault();
 })
 
 
-// const bookInfo = {}
 form.addEventListener("submit", (event) => {
-
   myLibrary.push(new Book(inputs[0].value, inputs[1].value, inputs[2].value, inputs[3].checked));
+  // console.log('myLibrary .push pass');
 
   addBookToLibrary()
 
   clearInput();
-
   event.preventDefault();
+
 })
 
 function clearInput() {
@@ -41,86 +37,88 @@ function clearInput() {
   inputs[3].checked = false
 
   dialog.close();
-
 }
 
 
 const myLibrary = [];
 
-function Book(bookName, authorName, numberOfPage, readStatus) {
-  // the constructor...
-  this.bookName = bookName
-  this.authorName = authorName
-  this.numberOfPage = numberOfPage
-  this.readStatus = readStatus
+// function Book(bookName, authorName, numberOfPage, readStatus) {
+//   // the constructor...
+//   this.bookName = bookName
+//   this.authorName = authorName
+//   this.numberOfPage = numberOfPage
+//   this.readStatus = readStatus
+// }
+
+
+class Book {
+  constructor(bookName, authorName, numberOfPage, readStatus) {
+      this.bookName = bookName;
+      this.authorName = authorName;
+      this.numberOfPage = numberOfPage;
+      this.readStatus = readStatus;
+  }
 }
 
-// let book1 = new Book('Ice and fire', 'keshu', 8, true);
-
-// myLibrary.push(book1)
-
-
 function addBookToLibrary() {
-  // do stuff here
   let tr = document.createElement('tr')
+  tr.setAttribute("data-bookNumber" , book)
   tBody.appendChild(tr);
 
-
-  let td1 = document.createElement('td')
-  tr.appendChild(td1);
-  td1.textContent = myLibrary[book].bookName
-  let td2 = document.createElement('td')
-  tr.appendChild(td2);
-  td2.textContent = myLibrary[book].authorName
-  let td3 = document.createElement('td')
-  tr.appendChild(td3);
-  td3.textContent = myLibrary[book].numberOfPage
-  let td4 = document.createElement('td')
-  tr.appendChild(td4);
-  // let td5 = document.createElement('td')
-  // const deleteBtn = document.createElement('button')
-  // td5.appendChild(deleteBtn)
-  // tr.appendChild(td5)
-
-  // td4.textContent = myLibrary[book].readStatus ? 'read' : 'not read'
-  // Create a select element
-  var selectElement = document.createElement("select");
-  selectElement.setAttribute("name", "read-status");
-  selectElement.setAttribute("id", "readStatus");
-
-  // Create the "not read" option
-  var notReadOption = document.createElement("option");
-  notReadOption.setAttribute("value", "not-read");
-  notReadOption.textContent = "Not Read";
-
-  // Create the "read" option
-  var readOption = document.createElement("option");
-  readOption.setAttribute("value", "read");
-  readOption.textContent = "Read";
-
-  // Append the options to the select element
-
-  if (myLibrary[book].readStatus) {
-    selectElement.appendChild(readOption);
-    selectElement.appendChild(notReadOption);
-  }
-  else {
-    selectElement.appendChild(notReadOption);
-    selectElement.appendChild(readOption);
-  }
-
-  td4.appendChild(selectElement);
+  // 
+  tr.innerHTML = `
+<td>${myLibrary[book].bookName}</td>
+<td>${myLibrary[book].authorName}</td>
+<td>${myLibrary[book].numberOfPage}</td>
+<td>
+    <select name="read-status" class="read-status">
+        <option value="not-read" ${myLibrary[book].readStatus ? '' : 'selected'}>Not Read</option>
+        <option value="read" ${myLibrary[book].readStatus ? 'selected' : ''}>Read</option>
+    </select>
+</td>
+<td>
+    <button class="delete-button" data-bookNumber = ${book}>Delete</button>
+</td>
+`;
 
 
-
+  // Add the event listener for the delete button in this function
+  const deleteButton = tr.querySelector(".delete-button");
+  console.log(deleteButton);
+  deleteButton.addEventListener("click", function () {
+      book--;
+      const bookNumber = this.getAttribute("data-bookNumber");
+      console.log(bookNumber);
+      // Call a function to remove the book from the library and the table
+      removeBookFromLibrary(bookNumber);
+  });
 
   book++;
 
-  // td.textContent = myLibrary[0]
-  // document.createElement('td')
-  // tr.appendChild(td);
-  // document.createElement('td')
-  // tr.appendChild(td);
-
 }
 
+
+// Function to remove a book from the library and table
+function removeBookFromLibrary(bookNumber) {
+  // Remove the book from the library array
+  myLibrary.splice(bookNumber, 1);
+
+  // Remove the corresponding row from the table
+  const rowToRemove = document.querySelector(`[data-bookNumber="${bookNumber}"]`);
+  console.log(rowToRemove);
+  if (rowToRemove) {
+      rowToRemove.remove();
+  }
+  reassignDataBookNumbers();
+}
+
+
+function reassignDataBookNumbers() {
+  
+  const rows = tBody.querySelectorAll("tr");
+  rows.forEach((row, index) => {
+      row.setAttribute("data-bookNumber", index);
+      const deleteButton = row.querySelector(".delete-button");
+      deleteButton.setAttribute("data-bookNumber", index);
+  });
+}
